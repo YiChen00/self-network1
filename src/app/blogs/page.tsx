7c334a7fda71c23f2b88b8828bfe32a0f5bbfd38
YiChen00 <3097549649@qq.com>
2025-03-22 22:36:46 +0800

    修改了app/blogs和project

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -4,7 +4,8 @@ import { Card } from '@/components/shared/Card'
 import { SimpleLayout } from '@/components/layout/SimpleLayout'
 import { type BlogType, getAllBlogs } from '@/lib/blogs'
 import { formatDate } from '@/lib/formatDate'
-import { blogHeadLine, blogIntro } from '@/config/infoConfig'
+
+// import { blogHeadLine, blogIntro } from '@/config/infoConfig'
 
 export const runtime = process.env.NEXT_RUNTIME === 'edge' ? 'edge' : 'nodejs'
 
@@ -37,27 +38,28 @@ function Blog({ blog }: { blog: BlogType }) {
   )
 }
 
-export const metadata: Metadata = {
-  title: 'Blogs',
-  description:
-    blogIntro
-}
+// export const metadata: Metadata = {
+//   title: 'Blogs',
+//   description:
+//     blogIntro
+// }
 
 export default async function BlogsIndex() {
   let blogs = await getAllBlogs()
 
   return (
-    <SimpleLayout
-      title={blogHeadLine}
-      intro={blogIntro}
-    >
-      <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
-        <div className="flex max-w-3xl flex-col space-y-16">
-          {blogs.map((blog: BlogType) => (
-            <Blog key={blog.slug} blog={blog} />
-          ))}
-        </div>
-      </div>
-    </SimpleLayout>
+    // <SimpleLayout
+    //   title={blogHeadLine}
+    //   intro={blogIntro}
+    // >
+    //   <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
+    //     <div className="flex max-w-3xl flex-col space-y-16">
+    //       {blogs.map((blog: BlogType) => (
+    //         <Blog key={blog.slug} blog={blog} />
+    //       ))}
+    //     </div>
+    //   </div>
+    // </SimpleLayout>
+    null
   )
 }
